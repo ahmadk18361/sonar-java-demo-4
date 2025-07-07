7285589da406194b47208a104aea57ff5c7c5f5d
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-07 13:15:24 -0400

    Create VulnerableExampleTest.java

---
+++ src/test/java/com/example/VulnerableExampleTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class VulnerableExampleTest {
+
+    @Test
+    public void testMain() {
+        // This runs the main method, which simulates the vulnerable code
+        String[] args = {};
+        VulnerableExample.main(args);
