f9bec3986356b4e47cce188979dbcf7dced6d43c
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-08 12:54:17 -0400

    Create CommonsIOCVE2021_33813Example.java

---
+++ src/main/java/com/example/CommonsIOCVE2021_33813Example.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.zip.*;
+
+public class CommonsIOCVE2021_33813Example {
+    public static void main(String[] args) throws IOException {
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream("sample.zip"));
+        ZipEntry entry = zipIn.getNextEntry();
+
+        while (entry != null) {
+            File outFile = new File("output/" + entry.getName()); // ❌ Vulnerable: No path validation
+            new FileOutputStream(outFile); // Could overwrite sensitive files
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+
+        zipIn.close();
+    }
+}
