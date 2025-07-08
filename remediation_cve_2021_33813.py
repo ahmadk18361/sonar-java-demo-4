a73538c01c994e8ad0df92640308db37088b064a
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-08 13:14:53 -0400

    Create remediation_cve_2021_33813.py

---
+++ remediation_cve_2021_33813.py
@@ -0,0 +1,21 @@
+import re
+
+with open("ZipSlipCVE2021_33813Example.java", "r") as file:
+    code = file.read()
+
+# Add path normalization check to prevent Zip Slip
+if 'normalize' not in code:
+    fix = '''
+    File destFile = new File(destDir, zipEntry.getName());
+    String destDirPath = destDir.getCanonicalPath();
+    String destFilePath = destFile.getCanonicalPath();
+    if (!destFilePath.startsWith(destDirPath + File.separator)) {
+        throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+    }
+    '''
+    code = code.replace("while ((zipEntry = zis.getNextEntry()) != null) {", 
+                        "while ((zipEntry = zis.getNextEntry()) != null) {
+" + fix)
+
+with open("ZipSlipCVE2021_33813Example.java", "w") as file:
+    file.write(code)
