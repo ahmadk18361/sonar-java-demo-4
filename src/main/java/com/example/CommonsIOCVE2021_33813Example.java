4461763fe3a32044998fadcedf4a0117acf45786
ahmadk18361 <ahmadk18361@gmail.com>
2025-07-11 16:17:17 -0400

    Update CommonsIOCVE2021_33813Example.java

--- src/main/java/com/example/CommonsIOCVE2021_33813Example.java
+++ src/main/java/com/example/CommonsIOCVE2021_33813Example.java
@@ -1,6 +1,6 @@
 import org.apache.log4j.Logger;
 
-public class CVE2021_27568 {
+public class CommonsIOCVE2021_33813Example {
     static Logger logger = Logger.getLogger(CVE2021_27568.class);
 
     public static void main(String[] args) {
